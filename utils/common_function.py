301d7a14a766d1df282c7dcc5c2ff38444d28f00
yasiruLakruwan <yapa1459@gmail.com>
2025-08-17 00:06:53 +0530

    Create the setup and added exeption and logging

---
+++ utils/common_function.py
@@ -0,0 +1,41 @@
+## Common functions for the project 
+import yaml
+import os
+from src.custom_exception import CustomExeption
+from src.logging import get_logger
+import pandas as pd
+
+logger = get_logger(__name__)
+
+# function for reading yamal file 
+
+def read_yaml_file(filepath):
+    try:
+        logger.info("Reading config.yaml file")
+        if not os.path.exists(filepath):
+            raise FileNotFoundError("File is not in the directry...!")
+        
+        with open(filepath,'r') as f:
+            config = yaml.safe_load(f)
+            return config
+        
+    except Exception as e:
+        logger.error("Error hapenign loading yaml file.....")
+        raise CustomExeption("Error loading yaml file",e)
+    
+# function to read csv file
+
+def read_csv_file(filepath):
+    try:
+        logger.info("Srarting reading csv file")
+
+        if not os.path.exists(filepath):
+
+            logger.error('CSV file not in the directry..')
+            raise FileNotFoundError("File not in the given directry....!")
+        
+        return pd.read_csv(filepath)
+    
+    except Exception as e:
+        logger.error("CSV file not loaded...")
+        raise CustomExeption("CSV file not loaded..",e)
