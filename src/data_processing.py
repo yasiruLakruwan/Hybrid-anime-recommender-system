6c5a2fc81662c1fbdb5efdbee4e2df277b8d4f24
yasiruLakruwan <yapa1459@gmail.com>
2025-08-24 08:54:56 +0530

    Data processing doing

--- src/data_processing.py
+++ src/data_processing.py
@@ -27,3 +27,31 @@ class DataProcessor:
         self.anime2anime_encoded = {}
         self.anime2anime_decoded = {}
 
+        os.makedirs(self.output_dir,exist_ok=True)
+        logger.info("Starting data loading...............")
+
+    def load_data(self,usecols):
+        try:
+            self.rating_df = pd.read_csv(self.input_file,low_memory=True,usecols=usecols)
+            logger.info("Data loaded successfully for data processing.........")
+        except Exception as e:
+            raise CustomExeption("Failed to load data..",sys)
+    
+    def filter_users(self,min_rating=50):
+        try:
+            n_ratings = self.rating_df["user_id"].value_counts()
+            self.rating_df = self.rating_df[self.rating_df["user_id"].isin(n_ratings[n_ratings>=50].index)].copy()
+            logger.info("Data filtered successfully.......")
+        except Exception as e:
+            raise CustomExeption("Faild to filter data.......",sys)
+        
+    def scale_ratings(self):
+        try:
+            min_rating =min(self.rating_df["rating"])
+            max_rating =max(self.rating_df["rating"])
+
+            self.rating_df["rating"] = self.rating_df["rating"].apply(lambda x: (x-min_rating)/(max_rating-min_rating)).values.astype(np.float64)
+            logger.info("Data scaling is done..........")
+        except Exception as e:
+            raise CustomExeption("Faild to Scale data.......")            
+
