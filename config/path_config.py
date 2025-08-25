e24b8b4ecdde68b21f14bf48e8fc442e0e0bb958
yasiruLakruwan <yapa1459@gmail.com>
2025-08-25 21:34:40 +0530

    commit

--- config/path_config.py
+++ config/path_config.py
@@ -21,3 +21,9 @@ RATING_DF = os.path.join(PROCESSED_DIR,"rating_df.csv")
 DF = os.path.join(PROCESSED_DIR,"anime_df.csv")
 SYNOPSIS_DF = os.path.join(PROCESSED_DIR,"synopsis_df.csv")
 
+USER2USER_ENCODED = "artifacts/processed/user2user_encoded.pkl"
+USER2USER_DECODED = "artifacts/processed/anime2anime_decoded.pkl"
+
+ANIME2ANIME_ENCODED = "artifacts/processed/anime2anime_encoded.pkl"
+ANIME2ANIME_DECODED = "artifacts/processed/anime2anime_decoded.pkl"
+
